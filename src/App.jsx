9407a6bdaaa401e893cfc65089f041568dcea43c
jemse123 <reyanmerwan42@gmail.com>
2024-12-09 03:11:21 +0300

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Sinin from './pages/Sinin'
+import Sinup from './pages/Sinup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <h1 className='text-center text-blue-400 text-3xl'>App</h1>
+   <BrowserRouter>
+   <Header/>
+   <Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='/about' element={<About />}/>
+<Route path='/sin-in' element={<Sinin/>}/>
+<Route path='/sin-up' element={<Sinup/>}/>
+<Route path='/dashboard' element={<Dashboard/>}/>
+<Route path='/project' element={<Projects/>}/>
+
+   </Routes>
+   
+   
+   
+   </BrowserRouter>
   )
 }
